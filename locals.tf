61b48a3e3465130807f8c00c8c85fc205de7c624
Kiril Soshnin <me@kirilsoshnin.com>
2024-10-06 13:01:42 -0400

    feat: add aws repos

--- locals.tf
+++ locals.tf
@@ -1,16 +1,31 @@
 locals {
   repos = {
     "ks-eci-terraform-tfe" = {
-      description        = "Automation for Terraform Enterprise"
+      description        = "Example Automation for Terraform Enterprise"
       gitignore_template = "Terraform"
       name               = "ks-eci-terraform-tfe"
       topics             = ["terraform"]
       visibility         = "public"
     }
     "ks-eci-terraform-github" = {
-      description        = "Automation for GitHub"
+      description        = "Example Automation for GitHub"
       gitignore_template = "Terraform"
       name               = "ks-eci-terraform-github"
+      topics             = ["terraform"]
+      visibility         = "public"
+    }
+    "ks-eci-terraform-aws-network" = {
+      description        = "Example Automation for AWS Networks"
+      gitignore_template = "Terraform"
+      name               = "ks-eci-terraform-aws-network"
+      topics             = ["terraform"]
+      visibility         = "public"
+    }
+
+    "ks-eci-terraform-aws-cluster" = {
+      description        = "Example Automation for AWS Clusters"
+      gitignore_template = "Terraform"
+      name               = "ks-eci-terraform-aws-cluster"
       topics             = ["terraform"]
       visibility         = "public"
     }
